f0edb7ca8554b5b0363b34858e159f6532bb6741
xaeroxaero <p69hnjd1>
2017-05-02 17:27:03 +0200

    blog paginating

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -23,7 +23,7 @@ function($scope, $http, $products, $blog, $document) {
             {
                 var someElement = angular.element(document.getElementById(id));
                 if (id==='contact'){
-                $document.duScrollToElementAnimated(someElement, -174)
+                $document.duScrollToElementAnimated(someElement, -175)
                 }
                 else {
                     $document.duScrollToElementAnimated(someElement)
